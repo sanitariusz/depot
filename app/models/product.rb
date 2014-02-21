d1490b87585b58bab65dcdeabb97958ce23583cb
sanitariusz <zetixs@gmail.com>
2014-02-21 16:29:26 +0100

    update_1

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,11 @@
 class Product < ActiveRecord::Base
+  #scope
+  default_scope :order => 'title'
+
+  #validations
+  validates :title, :description, :image_url, :presence => true
+  validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
+  validates :title, :uniqueness => true
+  validates :image_url, :format => {:with => /\.(gif|jpg|png)/i,
+                                    :message => 'bad URL format GIF, JPG, PNG.'}
 end
